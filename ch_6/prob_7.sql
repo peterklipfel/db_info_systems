0ccd170d812493a9bd1a06eb16f1964c43d25773
Peter Klipfel <peter@klipfel.me>
2013-10-08 18:28:44 -0600

    problem 7 solution with subqueries

--- ch_6/prob_7.sql
+++ ch_6/prob_7.sql
@@ -1,5 +1,8 @@
-SELECT vendor_name, invoice_number, MIN(invoice_date), invoice_total
+SELECT vendor_name, invoice_number, invoice_date, invoice_total
 FROM vendors v
   JOIN invoices i
     ON i.vendor_id=v.vendor_id
-GROUP BY vendor_name
+WHERE invoice_date=(
+  SELECT MIN(invoice_date)
+  FROM invoices
+  WHERE vendor_id=i.vendor_id)
